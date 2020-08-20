191882ccc85f216c7bd2fa91faa079b840953b85
MKJS <2362039373@qq.com>
2020-08-20 20:01:33 +0800

    ''

--- src/class/js/containers/App.js
+++ src/class/js/containers/App.js
@@ -88,7 +88,7 @@ class App extends Component {
                 if (data) {
                   if (hash.includes("Import")) {
                     //导入界面
-                    if (hash.includes("Student")) {
+                    if (!hash.includes("Student")) {
                       window.location.href = "/html/admclass#/Class"; //直接去到最新的版本
                       return;
                     }
