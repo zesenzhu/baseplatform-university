ba1c0705c1c06128662e2c2414f0c676eaeaa06c
zhangyang <zhangyangllx@163.com>
2020-08-21 14:36:40 +0800

    2020-08-21 14:36

--- src/common/Frame.js
+++ src/common/Frame.js
@@ -92,7 +92,7 @@ class FrameContainer extends Component{
 
                         const { UserName,PhotoPath } = JSON.parse(sessionStorage.getItem("UserInfo"));
 
-                        this.setState({UserInfo:{name:UserName,image:PhotoPath,childrenLoad:true}});
+                        this.setState({UserInfo:{name:UserName,image:PhotoPath},childrenLoad:true});
 
                         if (pageInit){
 
@@ -121,7 +121,7 @@ class FrameContainer extends Component{
 
                                 const { UserName,PhotoPath } = JSON.parse(sessionStorage.getItem("UserInfo"));
 
-                                this.setState({UserInfo:{name:UserName,image:PhotoPath,childrenLoad:true}});
+                                this.setState({UserInfo:{name:UserName,image:PhotoPath},childrenLoad:true});
 
                                 if (pageInit){
 
