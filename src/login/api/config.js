b3ba86f498e91fa20623f7459306b273a1d55d51
zhangyang <zhangyangllx@163.com>
2020-09-10 11:09:30 +0800

    2020-09-10 11:10

--- src/login/api/config.js
+++ src/login/api/config.js
@@ -5,9 +5,9 @@ if (process.env.NODE_ENV === 'development'){
 
     config = {
 
-        GetBaseInfo:'http://192.168.2.202:7300/mock/5d772752ed0ccd1564c8df0d/login',
+        //GetBaseInfo:'http://192.168.2.202:7300/mock/5d772752ed0ccd1564c8df0d/login',
 
-        //GetBaseInfo:'http://192.168.129.3:30103',
+        GetBaseInfo:'http://192.168.129.3:30103',
 
         Login:'http://192.168.129.3:30103',
 
