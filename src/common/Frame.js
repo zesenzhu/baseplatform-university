261b79ccc8556d0e4435865545988ab2cd3686a3
zhangyang <zhangyangllx@163.com>
2020-10-24 15:55:40 +0800

    2020-10-24 15:55

--- src/common/Frame.js
+++ src/common/Frame.js
@@ -447,6 +447,8 @@ class FrameContainer extends Component{
 
         const { children,onRef, pageInit,type,register,showBarner,showTop, showBottom, module, userInfo, msg, showLeftMenu,contentShow,onLogOut, ...reset } = this.props;
 
+        console.log(userInfo);
+
         return (
 
             <React.Fragment>
